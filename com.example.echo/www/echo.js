7767950c281071825f8f11a0be8b3fca1dd9a160
vbuzoverya <vvbuzove@VVBUZOVE-MOBL2.ccr.corp.intel.com>
2013-12-16 20:02:30 +0400

    Removing the unzipped package

--- com.example.echo/www/echo.js
+++
@@ -1,6 +0,0 @@
-
-window.echoPlugin = {
-  callNativeFunction: function(str, successCallback, errorCallback) {
-    return cordova.exec(successCallback, errorCallback, "EchoPlugin", "echo", [str]);
-  }
-}
